ed6ca5ba2c5bf7cebf317a4ba39c875ae701ce31
KacperKwiat <91725306+KacperKwiat@users.noreply.github.com>
2021-10-12 16:33:25 +0200

    Delete s01-mul-stdin.cpp

--- s01-mul-stdin.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-auto main() -> int
-{
-    auto const a = ask_user_for_integer("a=  ");
-    auto const b = ask_user_for_integer("b=  ");
-    std::cout << (a * b) << "\n";
-    return 0
-}
