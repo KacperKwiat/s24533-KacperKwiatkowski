b5a73d6d7c570830729b00a283b0e6566a6cfd15
KacperKwiat <91725306+KacperKwiat@users.noreply.github.com>
2021-10-12 16:31:30 +0200

    Create s01-sub-stdin.cpp

---
+++ s01-sub-stdin.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+auto main() -> int
+{
+    auto const a = ask_user_for_integer("a=  ");
+    auto const b = ask_user_for_integer("b=  ");
+    std::cout << (a / b) << "\n";
+    return 0
+}
