2eddc65fad14b56c1e38a21433f942faa1c7fa16
Kacper Kwiatkowski <s24533@pjswtk.deu.pl>
2021-11-18 18:41:02 +0100

    student

---
+++ src/s03-student.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+struct Student
+{
+	private:
+	string imie;
+	string nazwisko;
+	string indeks;
+	int semestr;
+	float srednia;
+	public:
+	Student(string im, string naz, string in, int sm, float sr)
+	{
+		if(im=="" || naz=="" || in=="" || sm<=0 || sr<0) cout<<"Podales bledne dane";
+		else {imie=im; nazwisko=naz; indeks=in; semestr=sm; srednia=sr;}
+	}
+	void pokaz()
+	{
+		cout<<imie<<" "<<nazwisko<<" "<<indeks<<" "<<semestr<<" "<<srednia<<"\n";
+	}
+};
+int main()
+{
+	Student s1("Kacper","Kwiatkowski","s24533",1, 4.2);
+	s1.pokaz();
+	return 0;
+}
+	
